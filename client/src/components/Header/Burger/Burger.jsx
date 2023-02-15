1d06be3c5b3810a8e4539d39cbb012013e5bbcde
Katerina Shevchenko <katyusha.shev18@gmail.com>
2023-02-15 23:16:42 +0200

    fixed burger

--- client/src/components/Header/Burger/Burger.jsx
+++ client/src/components/Header/Burger/Burger.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import styles from './Burger.module.scss'
 import PropTypes from 'prop-types'
 
 const Burger = ({ onClick }) => {
-  const { location } = useSelector(state => state.location)
-
   return (
-    <div className={location !== '/' ? styles.burger_page : styles.burger}>
+    <div className={styles.burger}>
       <div className={styles.burger_btn} onClick={onClick}>
         <div className={styles.burger_btn_line} />
         <div className={styles.burger_btn_line} />
